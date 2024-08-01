8979fccaefcdabb1fc1d7f07d39aceb5b3fcdff5
Reysbel Cruz <raydencm05@gmail.com>
2024-08-01 14:21:02 -0400

    se cambio el nombre de la empresa

--- resources/views/cliente/layouts/navbar.blade.php
+++ resources/views/cliente/layouts/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container px-4 px-lg-5">
-        <a class="navbar-brand" href="#!">El Buen Sazon</a>
+        <a class="navbar-brand" href="#!">la casa del libro</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
